d64cf322364a59896a3008856793244a36f936f2
AlanT <alanhstam@gmail.com>
2026-09-05 08:50:28 +0800

    first commit

---
+++ City maximiser.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import random #python libraries used in the program
+
+class District:
+    def __init__(self, name, symbol, cost, upkeep, base_score): #Base attributes of the district
+        self.name = name
+        self.symbol = symbol
+        self.cost = cost
+        self.upkeep = upkeep
+        self.base_score = base_score
+    def get_details(self):  #Prints detail and attributes of the district
+        return f"District Name: {self.name}, Symbol: {self.symbol}, Cost: {self.cost}, Upkeep: {self.upkeep}, Base Score: {self.base_score}"
+
+class Residential(District): #Residential subclass
+    def __init__(self, name, symbol, cost, upkeep, base_score):
+        super().__init__(name, symbol, cost, upkeep, base_score)
+        self.population = 50
+    def get_details(self):
+        return f"{super().get_details()}"
+    
+class Commercial(District): #Commercial subclass
+    def __init__(self, name, symbol, cost, upkeep, base_score):
+        super().__init__(name, symbol, cost, upkeep, base_score)
+        self.revenue = 100
+    def get_details(self):
+        return f"{super().get_details()}"
+      
+class industrial(District): #industrial subclass
+    def __init__(self, name, symbol, cost, upkeep, base_score):
+        super().__init__(name, symbol, cost, upkeep, base_score)
+        self.revenue = 300
+    def get_details(self):
+        return f"{super().get_details()}"
+
+class Recreational(District): #recreational subclass
+    def __init__(self, name, symbol, cost, upkeep, base_score):
+        super().__init__(name, symbol, cost, upkeep, base_score)
+    def get_details(self):
+        return f"{super().get_details()}"
+    
+class CityGrid: #Object representing the city grid
+    def __init__(self, width, height, grid):
+        self.width = width 
+        self.height = height
+        self.grid = grid  # 2D list representing the city grid
+    def grid_render(self): #Prints the grid in a readable format
+        for row in self.grid:
+            print(" . ".join(row))
+    def is_empty(self, x, y): #Checks if a specific cell in the grid is empty or occupied
+        if self.grid[y][x] == '.': #Checks for an empty cell at a specific coordinate
+            return "Empty"
+        else:
+            return "Occupied"
+            
